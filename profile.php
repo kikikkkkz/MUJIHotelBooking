30bdb54d35b2af32d97eb9439651780d64423142
Unknown <jennyyyzou@hotmail.com>
2018-04-02 20:12:34 -0700

    Merged files properly for profile.php

--- profile.php
+++ profile.php
@@ -2,9 +2,6 @@
 require_once('initialize.php');
 $id=$_SESSION['admin_id'];
 
-$page_title = 'Profile';
-include('header.php');
-
 //select all properties from roomtype 
 $query_str = "SELECT lastName,firstName,email,phoneNumber,country FROM members WHERE memberNumber = ?";
 			  
@@ -13,13 +10,13 @@ $stmt->bind_param('i',$id);
 $stmt->execute();
 $stmt->bind_result($last1,$first1,$email1,$phone1,$country1);;
 
-<<<<<<< HEAD
+
 $page_title = 'Profile';
 include('header.php');
 
 echo "<div class=\"login\">";
 
-//display results
+//display profile 
 if($stmt->fetch()) {
 	echo "<h4>Hello, $first1 $last1!</h4>\n";
 	echo "<table cellspacing=5><p>";
@@ -30,71 +27,46 @@ if($stmt->fetch()) {
 }
 $stmt->free_result();
 
-echo "<p>";
-echo "<b>Your Reservation</b><br>";
-
-$query_str = "SELECT bookingNumber, checkInDate, checkOutDate, priceEach, roomType FROM reservation WHERE memberNumber=?";
-
-$stmt = $db->prepare($query_str);
-$stmt->bind_param('i',$id);
-$stmt->execute();
-$stmt->bind_result($booking1,$checkin1,$checkout1,$price1,$room1);;
-
-echo "<table cellspacing=5>";
-if($stmt->fetch()) {
-	echo "<tr><td>Booking Number <b>$booking1</b></td> <td>|</td> <td>$checkin1 ~ $checkout1</td></tr>";
-	echo "<tr><td></td> <td>|</td> <td>Type $room1</td></tr>";
-	echo "<tr><td></td> <td>|</td> <td>RMB $price1 /night</td></tr>";
-}
-echo "</p></table>";
-
-
-$stmt->free_result();
-=======
-//display results
-if($stmt->fetch()) {
-	echo "<h4>Hello, $first1 $last1!</h4>\n";
-	echo "<p>";
-	echo "<strong>Your Profile</strong><br>";
-	echo "Email: $email1<br>
-	Phone Number: $phone1 <br>
-	Country: $country1<br><br>";
-	echo "</p>";
-}
-$stmt->free_result();
-
 $query_str = "SELECT bookingNumber, checkInDate, checkOutDate, priceEach, roomType FROM reservation WHERE memberNumber=".$id."";
 			  
 $res = $db->query($query_str);
 
+//display reservation
 echo "<p>";
-echo "<strong>Your Reservation</strong><br>";
+echo "<b>Your Reservation</b><br>";
 if($res->num_rows > 0) {
 	while ($row = $res->fetch_assoc()) {
-		echo "Booking Number ".$row['bookingNumber']." | ".$row['checkInDate']." ~ ".$row['checkOutDate']."<br>";
-		echo "Type ".$row['roomType']." RMB ".$row['priceEach']."/night<br>";
+		// echo "Booking Number ".$row['bookingNumber']." | ".$row['checkInDate']." ~ ".$row['checkOutDate']."<br>";
+		// echo "Type ".$row['roomType']." RMB ".$row['priceEach']."/night<br>";
+		echo "<table cellspacing=5>";
+		echo "<tr><td>Booking Number <b>".$row['bookingNumber']."</b></td> <td>|</td> <td>".$row['checkInDate']."~ ".$row['checkOutDate']."</td></tr>";
+		echo "<tr><td></td> <td>|</td> <td><b><a href=";
+		echo url_for('roomdetails.php?room='.$row['roomType']);
+		echo ">Type ".$row['roomType']."</b></a></td></tr>";
+		echo "<tr><td></td> <td>|</td> <td>RMB ".$row['priceEach']." /night</td></tr>";
+		echo "</p></table>";
+
+		
 	}
 }else{
 	echo "Haven't made any reservations yet."; //show 0 result it there is nothing matched 
 }
 echo "</p>";
 $res->free_result();
->>>>>>> master
+
 
 echo "<br>";
 
-//print out comments posted by the member
+//display comments posted by the member
 $query_str = "SELECT content, timePosted, roomType FROM comment WHERE memberNumber = ".$id."";
 			  
 $res = $db->query($query_str);
 
 echo "<p>";
-<<<<<<< HEAD
+
 echo "<b>Your Comments</b><br>";
 echo "<table cellspacing=5>";
-=======
-echo "<strong>Your Comment</strong><br>";
->>>>>>> master
+
 if($res->num_rows > 0) {
 	while ($row = $res->fetch_assoc()) {
 		echo "<tr><td>".$row['timePosted']."</td> <td>|</td>"; 
